b4958b92ee68938dd3fbe820893ed0d91f47051c
kemojijyothi <134859000+kemojijyothi@users.noreply.github.com>
2023-05-28 23:23:00 +0530

    using char and char dataype

--- day1/demo.java
+++ day1/demo.java
@@ -1,18 +1,17 @@
-//all artimetic operators using char and number type
+//all artimetic operators using char and number
 class Demo
 {
  public static void main(String [] args)
  {
     int num1=20;
     char char1='a';
-    char char2='b';
     System.out.println(num1+char1);
     System.out.println(num1 + " adding number and char "+ char1+" = " +(num1+char1));
-    System.out.println(char1 + " adding two characters "+ char2+ "= "+(char1+char2));
-    System.out.println(char1 + " subtracting two  characters "+ char2+ "= "+(char1-char2));
-    System.out.println(char1 + " multiplication two characters "+ char2+ "= "+(char1*char2));
-    char char3='d';
-    System.out.println(char1 + " division two characters "+ char3 + "= "+(char1/char3));
-    System.out.println(char1 + " module two characters "+ char3+ "= "+(char1%char3));
+    System.out.println(num1 + " subtracting number and char "+ char1+" = " +(num1-char1));
+    System.out.println(num1 + " multipication number and char "+ char1+" = " +(num1*char1));
+    System.out.println(num1 + " division of number and char "+ char1+" = " +(num1/char1));
+    System.out.println(num1 + " module of number and char "+ char1+" = " +(num1%char1));
+    
+    
  }
 }
---
+++ day1/demochar.java
@@ -0,0 +1,20 @@
+//all artimetic operators using char and char type
+class Demochar
+{
+ public static void main(String [] args)
+ {
+    
+    char char1='a';
+    char char2='b'; 
+    System.out.println(char1 + " adding  two characters "+ char2 + "= "+(char1+char2));
+    System.out.println(char1 + " adding two characters "+ char2+ "= "+(char1+char2));
+    System.out.println(char1 + " subtracting two  characters "+ char2+ "= "+(char1-char2));
+    System.out.println(char1 + " multiplication two characters "+ char2+ "= "+(char1*char2));
+    char char3='d';
+    System.out.println(char1 + " division two characters "+ char3 + "= "+(char1/char3));
+    System.out.println(char1 + " module two characters "+ char3+ "= "+(char1%char3));
+    //char char1='t'; dublicate variable not used in with in the main block
+    System.out.println(char1 + " module two characters "+ char3+ "= "+(char1%char3));
+
+ }
+}
