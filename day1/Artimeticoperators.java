423e94bb88f0ea01710aecf8549cd2eca2348ab4
kemojijyothi <134859000+kemojijyothi@users.noreply.github.com>
2023-05-28 21:22:19 +0530

    concept: Artimeticoperators

---
+++ day1/Artimeticoperators.java
@@ -0,0 +1,15 @@
+class Artimeticoperators
+{
+    public static void main(String [] args)
+    {
+        int a=30;
+        int b=50;
+        
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+
+    }
+
+}
