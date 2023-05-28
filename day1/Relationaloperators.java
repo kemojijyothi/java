f40746f556e4b1f54c6d56b1b31ee721c1018b22
kemojijyothi <134859000+kemojijyothi@users.noreply.github.com>
2023-05-28 21:59:52 +0530

    relational operators

---
+++ day1/Relationaloperators.java
@@ -0,0 +1,21 @@
+class Relationaloperators
+{
+    public static void main(String [] args)
+    {
+        int num1=20;
+        int num2=50;
+        System.out.println(num1 +" greater than "+ num2 +" = "+(num1>num2));
+        System.out.println(num1 +" is less than "+num2 + " = "+ (num1<num2));
+        System.out.println(num1 + " eual to "+ num2 + " ="+ (num1==num2));
+        num2=20;
+        System.out.println(num1 + " equal to "+ num2 + " ="+ (num1==num2));
+        num1=30;
+        System.out.println(num1 + " not  equal to "+ num2 + " ="+ (num1!=num2));
+        System.out.println(num1 + " greater than  equal to "+ num2 + " ="+ (num1<=num2));
+        System.out.println(num1 + " less than  eqal to "+ num2 + " ="+ (num1>=num2));
+
+        
+    }
+}
+
+
