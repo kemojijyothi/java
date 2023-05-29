8fb2a340fd35e51ce449f11b36e29f5fe127ac95
kemojijyothi <134859000+kemojijyothi@users.noreply.github.com>
2023-05-29 22:04:33 +0530

    problem2 logicaloperators

---
+++ day2/Logical2.java
@@ -0,0 +1,11 @@
+class Logical2
+{
+    public static void main(String [] args)
+    {
+        int a=2;
+        int b=10;
+        System.out.println((true||false)||(true&&true));
+        System.out.println(a==5&&b>10);
+        System.out.println(5==10&&10<50);
+    }
+}
