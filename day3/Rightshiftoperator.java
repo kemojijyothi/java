6a41cf654e8fe20ea51b26433fd6fddb532046fd
kemojijyothi <134859000+kemojijyothi@users.noreply.github.com>
2023-05-30 21:00:42 +0530

    ternary operator

---
+++ day3/Rightshiftoperator.java
@@ -0,0 +1,14 @@
+class Rightshiftoperator
+{
+    public static void main(String [] args){
+        String name=10<5?"jhanu":"jyothi";
+        System.out.println(name);
+        boolean results=20>10?false:true;
+        System.out.println(results);
+        int a=30<5?'g':'k';
+        System.out.println(a);
+        int m=3>10?5:10;
+        System.out.println(m);
+        
+    }
+}
