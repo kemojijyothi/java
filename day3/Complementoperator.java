619bb70574e4ca51a715c50773d96099ad80c3c1
kemojijyothi <134859000+kemojijyothi@users.noreply.github.com>
2023-05-30 19:34:04 +0530

    complimentoperator

---
+++ day3/Complementoperator.java
@@ -0,0 +1,10 @@
+class Complementoperator
+{
+    public static void main(String [] args){
+        int a=6;
+        System.out.println(~a);
+        int b=-7;
+        System.out.println(~b);
+        
+    }
+} 
