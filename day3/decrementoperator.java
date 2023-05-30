970751596a568da57019a703952ddcd22c3d4889
kemojijyothi <134859000+kemojijyothi@users.noreply.github.com>
2023-05-30 21:22:26 +0530

    decrement operator

---
+++ day3/decrementoperator.java
@@ -0,0 +1,11 @@
+class Decrementoperator
+{
+    public static void main(String [] args){
+        int a=5;
+        System.out.println(a--);
+        System.out.println(a--);
+        System.out.println(--a);
+        System.out.println(a--);
+        System.out.println(a);
+    }
+}
