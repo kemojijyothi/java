ec597539232b3217899bdc44a76be2268902df0c
kemojijyothi <134859000+kemojijyothi@users.noreply.github.com>
2023-05-30 19:40:04 +0530

    leftshifoperator

---
+++ day3/leftshiftoperator.java
@@ -0,0 +1,9 @@
+class leftshiftoperator{
+    public static void main(String [] args){
+        int n=7;
+        System.out.println(n<<2);//output=28
+        int m=6;
+        System.out.println(m<<3);//output=48
+        
+    }
+}
