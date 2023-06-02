4a2a09aa4dc9e3ea39fa7293e695ac83e3b0d956
kemojijyothi <134859000+kemojijyothi@users.noreply.github.com>
2023-06-02 19:27:29 +0530

    ternary operator

---
+++ day4/Name.java
@@ -0,0 +1,8 @@
+class Name{
+    public static void main(String [] args){
+        int x=20;
+        String sup=(x<15?"s":x>20?"h":"k");
+        System.out.println(sup);
+        
+    }
+}
---
+++ day4/operators.java
@@ -0,0 +1,19 @@
+class operators
+{
+    public static void main(String [] args){
+        int x=30;
+        int y=40;
+         int z=x--+y--;
+         z=x--*y--;
+         
+        z--;
+        System.out.println(x++);
+        System.out.println(--y);
+        System.out.println(z++);
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        
+        
+    }
+}
