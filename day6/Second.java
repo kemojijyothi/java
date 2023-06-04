5adedf997ff8328006f10a91c71420fe11ef21b5
kemojijyothi <134859000+kemojijyothi@users.noreply.github.com>
2023-06-04 13:01:45 +0530

    elseifcondition

---
+++ day6/Second.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Second
+{
+	public static void main (String[] args)
+	{
+	    int r;
+        int k;
+        r = 42;
+        k = 32;
+        if (r > k) {
+            System.out.println("Ram is heavier than Karan.");
+        } else if (r < k) {
+            System.out.println("Karan is heavier than Ram");
+        } else {
+            System.out.println("Ram & Karan have the same weight!");
+        }
+
+        r = 88;
+        k = 78;
+        if (r > k) {
+            System.out.println("Ram is heavier than Karan.");
+        } else if (r < k) {
+            System.out.println("Karan is heavier than Ram");
+        } else {
+            System.out.println("Ram & Karan have the same weight!");
+        }
+	}
+}
