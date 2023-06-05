d29c11f4ca2feb3bc1cb5b947a46f28362c7f163
kemojijyothi <134859000+kemojijyothi@users.noreply.github.com>
2023-06-05 22:20:32 +0530

    switchoperator

---
+++ day6/Switchcondiotion.java
@@ -0,0 +1,15 @@
+class Switchcondiotion{
+    public static void main(String [] args){
+        int ch=10;
+            switch(ch)
+            {
+                case 3:System.out.println(3);
+                break;
+                case 5*2:System.out.println(56);
+                break;
+                case 1:System.out.println(1);
+                break;
+                default:System.out.println("inavalid");  
+            }
+    }
+}
