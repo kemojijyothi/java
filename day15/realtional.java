f65b4117100f6076ca75bf0dd4b4f3b04f32adee
kemojijyothi <134859000+kemojijyothi@users.noreply.github.com>
2023-06-18 12:56:13 +0530

    relationaloperators

---
+++ day15/realtional.java
@@ -0,0 +1,15 @@
+//retational oparator out shpuld be boolean
+class RealtionalOparator{
+    public static void main(String [] args){
+        int a=20;
+        int b=60;
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a<=b);
+        System.out.println(a>=b);
+        
+        System.out.println(a!=b);
+        System.out.println(a==b);
+        
+    }
+}
