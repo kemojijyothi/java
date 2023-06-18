b82734061703de6aa2e3026d94740d9b8e6edcd4
kemojijyothi <134859000+kemojijyothi@users.noreply.github.com>
2023-06-18 12:52:40 +0530

    example

---
+++ day6/alphabets.java
@@ -0,0 +1,12 @@
+//w.a.print the alphabets with its position
+class Aplhabets{
+public static void main(String [] args)
+{
+   for(int i=1;i<=26;i++)
+	{
+			for(char ch=A;ch<=Z; char++)
+			{
+					System.out.println(ch+"i")
+			}
+	}
+}
