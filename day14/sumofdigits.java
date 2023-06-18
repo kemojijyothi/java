2e9629dfb95fa9a80162675c61eadc22a0bbb693
kemojijyothi <134859000+kemojijyothi@users.noreply.github.com>
2023-06-18 12:54:10 +0530

    sumofdidgits

---
+++ day14/sumofdigits.java
@@ -0,0 +1,18 @@
+//write a program to print the sum of the digits from the given number
+import java.util.Scanner;
+class Sumofdigits{
+public static void main(String [] args)
+{
+	Scanner scan=new Scanner(System.in);
+        System.out.println("enter the number");
+	int num=scan.nextInt();
+	int sum=0;
+	while(num>0)
+	{
+		int rem=num%10;
+		sum=sum+rem;
+		num=num/10;
+	}
+	System.out.println(sum);
+}
+}
