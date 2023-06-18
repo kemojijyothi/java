2c14bd451eef05f68aa3497595260bac9b0a120f
kemojijyothi <134859000+kemojijyothi@users.noreply.github.com>
2023-06-18 12:56:29 +0530

    sumofdidgits

---
+++ day14/sum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Sumofdigits{
+public static void main(String [] args)
+{
+	Scanner scan=new Scanner(System.in);
+        System.out.println("enter the number");
+	int num=scan.nextInt();
+	int sum=0;
+	while(num>0)
+	{
+		int rem=num%10;
+		sum=sum+rem;
+		num=num/10;
+	}
+	System.out.println(sum);
+}
+}
