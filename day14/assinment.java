2a96f5b751260b80c9692034b4839c0d35a3985c
kemojijyothi <134859000+kemojijyothi@users.noreply.github.com>
2023-06-18 12:58:32 +0530

    enhancedswitch

---
+++ day14/assinment.java
@@ -0,0 +1,12 @@
+class Assinment{
+    public static void main(String [] args){
+        int num=16;
+        switch(num)
+    {
+        case 17-> System.out.println("hello");
+        case 16->System.out.println("good mrg");
+        case 18-> System.out.println("jytoji");
+        default->System.out.println("suretrust");
+    }
+    }
+}
