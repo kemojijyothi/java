9bc95633b079a5d2686c445b937da40fce36b154
kemojijyothi <134859000+kemojijyothi@users.noreply.github.com>
2023-06-18 12:53:15 +0530

    assignemnt

---
+++ day15/assigmnet.java
@@ -0,0 +1,9 @@
+class Assigment{
+    public static void main(String [] args){
+        int num=78;
+        int num2=80;
+        if(num!=num2){
+            System.out.println(num +" is not equal to "+num2);
+        }
+    }
+}
---
+++ day15/assignment.java
@@ -0,0 +1,20 @@
+//using assingment operator
+class AssignmentOperator
+{
+    public static void main(String [] args){
+        int a=90;
+        int b=47;
+        System.out.println(a);//90
+        System.out.println(b);//47
+         a+=5;//a=a+5==>a=90+5=95
+         System.out.println(a);//95
+         b/=10;//b=b/10==>b=47/10==>4
+         System.out.println(b);//4
+         b*=a;//b=b*a==>b=4*95=380
+         System.out.println(b);
+         a%=10;//a=a%10==>95%10==>5
+         System.out.println(a);
+
+
+    }
+}
